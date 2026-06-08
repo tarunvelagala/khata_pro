7592190f05cf051209fec36bc86c4a9c59da6460
Tarun Velagala <tarun.kumar.reddy.velagala@sap.com>
2026-06-08 20:13:59 +0530

    chore: production icons and splash screens for both platforms
    - New app icon SVGs (light, dark, monochrome, tinted, foreground variants)
    - flutter_launcher_icons regenerated for all Android densities (adaptive,
      monochrome, dark foreground) and all iOS sizes (standard, dark, tinted)
    - Android splash: GPay-style icon tile on always-dark surface; KhataPro
      wordmark added; light/dark/v31 styles updated; animated vectors added
    - iOS splash: LaunchScreen.storyboard rewritten with wordmark; launch images
      regenerated at 1x/2x/3x for light and dark; SplashBackground color token set

--- android/app/src/main/kotlin/com/vtkr/khata_pro/MainActivity.kt
+++ android/app/src/main/kotlin/com/vtkr/khata_pro/MainActivity.kt
@@ -1,5 +1,14 @@
 package com.vtkr.khata_pro
 
+import android.os.Build
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity : FlutterActivity()
+class MainActivity : FlutterActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            splashScreen.setOnExitAnimationListener { it.remove() }
+        }
+        super.onCreate(savedInstanceState)
+    }
+}
